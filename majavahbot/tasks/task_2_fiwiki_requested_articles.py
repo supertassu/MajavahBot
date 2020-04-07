bcf5ade55bcdcbc2e88447f4e10a9ecfa444e76e
Taavi Väänänen <hi@tassu.me>
2020-04-07 13:49:00 +0300

    More work on requested articles clerk

--- majavahbot/tasks/task_2_fiwiki_requested_articles.py
+++ majavahbot/tasks/task_2_fiwiki_requested_articles.py
@@ -7,6 +7,16 @@ ENTRY_REGEX = compile(r"\n:*\*+ ?([^\n]+)")
 LOCAL_LINK_REGEX = compile(r"\[\[([^\:\]]+)\]\]")
 EXISTING_PAGE_QUERY = "SELECT page_title FROM page WHERE page_namespace = 0 AND page_is_redirect = 0 AND page_title IN (%s)"
 
+# todo: add rest and move to config page
+PAGES = ['Wikipedia:Artikkelitoiveet/Tekniikka']
+
+# todo: move to a config page
+BLACKLISTED_TERMS = [
+    "<!-- keep -->",
+    "luettelo",
+    "punaiset linkit"
+]
+
 
 class FiwikiRequestedArticlesTask(Task):
     def process_page(self, page: str, api: MediawikiApi, replica: ReplicaDatabase):
@@ -18,23 +28,38 @@ class FiwikiRequestedArticlesTask(Task):
         for entry in entries:
             entry_text = entry.group(1)
             first_link = LOCAL_LINK_REGEX.match(entry_text)
-            if first_link:
-                requests[first_link.group(1).replace(" ", "_")] = entry.span()
+            if first_link and not any(term.lower() in entry_text.lower() for term in BLACKLISTED_TERMS):
+                requests[first_link.group(1).replace(" ", "_")] = entry.group(0)
 
         page_titles = requests.keys()
         format_strings = ','.join(['%s'] * len(page_titles))
         existing_pages = replica.execute(EXISTING_PAGE_QUERY % format_strings, tuple(requests.keys()))
 
-        print(existing_pages)
+        removed_entries = []
+        new_text = text
+
+        for existing_page in existing_pages:
+            existing_page = existing_page[0].decode('utf-8')
+            existing_page_entry = requests[existing_page]
+            new_text = new_text.replace(existing_page_entry, '')
+            removed_entries.append(existing_page)
+
+        if len(removed_entries) > 0 and self.should_edit():
+            if text == new_text:
+                raise RuntimeError("text == new_text but at least one entry should be removed")
+            # TODO: save
 
     def run(self):
         replicadb = ReplicaDatabase("fiwiki")
         replicadb.request()
 
         api = self.get_mediawiki_api()
-        self.process_page('Wikipedia:Artikkelitoiveet/Tekniikka', api, replicadb)
+
+        for page in PAGES:
+            print("Processing page", page)
+            self.process_page(page, api, replicadb)
 
         replicadb.close()
 
 
-# task_registry.add_task(FiwikiRequestedArticlesTask(2, 'Requested articles clerk', 'fi'))
+task_registry.add_task(FiwikiRequestedArticlesTask(2, 'Requested articles clerk', 'fi'))
